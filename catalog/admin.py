7679655ad50d209c3254870d900a80fbbdebe2e9
Kirill <khamenman@gmail.com>
2017-11-26 22:13:17 +0400

    admin intergration for catalog app

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('id', '__str__')
+    list_display_links = ('id', '__str__')
+    search_fields = ('id', 'name', 'family_name')
+
+
+@admin.register(models.Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'author', 'publisher')
+    list_display_links = ('id', 'title')
+    search_fields = ('id', 'title', 'title_original', 'isbn', 'author__name', 'author__family_name', 'publisher__name')
+
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('id', 'name')
+    search_fields = ('id', 'name')
+
+
+@admin.register(models.Publisher)
+class PublisherAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('id', 'name')
+    search_fields = ('id', 'name')
