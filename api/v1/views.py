95af10e43be5a7cf009d688c80ffc9fdfae85fba
Kirill <khamenman@gmail.com>
2017-11-29 19:19:21 +0400

    encapsulated expand functionality in a mixin

--- api/v1/views.py
+++ api/v1/views.py
@@ -3,19 +3,28 @@ from .serializers import AuthorSerializer, BookSerializer, ExpandedBookSerialize
 from catalog.models import Author, Book, Category
 
 
+class ExpandableViewSetMixin(viewsets.ModelViewSet):
+    serializer_expanded_class = None
+
+    def should_expand(self):
+        return self.request.GET.get('expand', False)
+
+    def get_serializer_class(self):
+        if self.should_expand():
+            return self.serializer_expanded_class
+
+        return super().get_serializer_class()
+
+
 class AuthorViewSet(viewsets.ModelViewSet):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
 
 
-class BookViewSet(viewsets.ModelViewSet):
+class BookViewSet(ExpandableViewSetMixin, viewsets.ModelViewSet):
     queryset = Book.objects.all()
-
-    def get_serializer_class(self):
-        if self.request.GET.get('expand', False):
-            return ExpandedBookSerializer
-
-        return BookSerializer
+    serializer_class = BookSerializer
+    serializer_expanded_class = ExpandedBookSerializer
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
