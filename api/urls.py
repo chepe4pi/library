2867ad32f336de67f1dcf5961ad9bb005933e2e8
Kirill <khamenman@gmail.com>
2017-11-30 20:35:12 +0400

    moved api auth urls

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,4 @@ from django.conf.urls import url, include
 
 urlpatterns = [
     url(r'^v1/', include('api.v1.urls', namespace='v1')),
-    url(r'^auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
--- library/urls.py
+++ library/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^rest-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^api/', include('api.urls', namespace='api')),
     url(r'^', include('catalog.urls'))
 ]
