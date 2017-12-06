63292a376f649d7f41c4c588d13a9852f152cc37
Kirill <khamenman@gmail.com>
2017-12-06 21:47:52 +0400

    test refactoring

---
+++ api/v1/tests/factories.py
@@ -0,0 +1,70 @@
+from factory.django import DjangoModelFactory
+import factory
+from catalog.models import Book, Author, Category, UserBookRelation
+from factory import Faker
+import random
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class UserFactory(DjangoModelFactory):
+    username = Faker('name')
+    password = Faker('password')
+    email = Faker('email')
+
+    class Meta:
+        model = User
+
+
+class AuthorFactory(DjangoModelFactory):
+    name = Faker('word')
+    family_name = Faker('word')
+    about = Faker('paragraph')
+
+    class Meta:
+        model = Author
+
+
+class CategoryFactory(DjangoModelFactory):
+    name = Faker('word')
+    description = Faker('paragraph')
+
+    class Meta:
+        model = Category
+
+
+class BookFactory(DjangoModelFactory):
+    title = Faker('word')
+    title_original = Faker('word')
+    author = factory.Iterator(Author.objects.all())
+    year_published = Faker('year')
+
+    @factory.post_generation
+    def categories(self, create, extracted, **kwargs):
+        if not create:
+            # Simple build, do nothing.
+            return
+
+        if extracted:
+            self.categories.add(extracted)
+            return
+
+        categories = Category.objects.all()
+        n = random.choice(range(categories.count()))
+        for i in range(n):
+            self.categories.add(random.choice(categories))
+
+    class Meta:
+        model = Book
+
+
+class UserBookRelationFactory(DjangoModelFactory):
+    book = factory.Iterator(Book.objects.all())
+    user = factory.Iterator(User.objects.all())
+    in_bookmarks = Faker('pybool')
+    in_wishlist = Faker('pybool')
+    rating = random.choice(UserBookRelation.RATING_CHOICES)[0]
+
+    class Meta:
+        model = UserBookRelation
--- api/v1/tests/tests.py
+++ api/v1/tests/tests.py
@@ -1,114 +1,100 @@
 from rest_framework.test import APITestCase
-from catalog.models import Author, Book, Category, UserBookRelation
+from catalog.models import Book
 from django.contrib.auth import get_user_model
-from rest_framework.test import APIClient
 from django.urls import reverse
-import status
+from .factories import UserFactory, BookFactory, UserBookRelationFactory, AuthorFactory, CategoryFactory
+from api.v1.serializers import BookSerializer
+import status, pdb, random
 
 User = get_user_model()
-client = APIClient()
 
 
 class BooksEndpointTestCase(APITestCase):
     def setUp(self):
-        admin = User.objects.create_superuser(username='admin', password='123123qwe', email='test@test.ru')
-        user = User.objects.create_user(username='user1', password='123123qwe')
-        category1 = Category.objects.create(name='Category1')
-        category2 = Category.objects.create(name='Category2')
-        category3 = Category.objects.create(name='Category3')
-        author1 = Author.objects.create(name='Author1', family_name='Author1FamilyName')
-        author2 = Author.objects.create(name='Author2', about='Author2About')
-        book1 = Book.objects.create(title='Book1', title_original='Book1Original', author=author1)
-        book1.categories.add(category1, category2)
-        book1.save()
-        book2 = Book.objects.create(title='Book2', title_original='Book2Original', author=author2)
-        book2.categories.add(category2, category3)
-        book2.save()
+        self.admin = UserFactory.create(is_superuser=True)
+        self.user = UserFactory.create(is_superuser=False)
 
-        UserBookRelation.objects.create(user=admin, book=book2, in_bookmarks=True, in_wishlist=True,
-                                        rating=UserBookRelation.RATING_GOOD)
-        UserBookRelation.objects.create(user=user, book=book2, in_bookmarks=True, in_wishlist=True,
-                                        rating=UserBookRelation.RATING_GOOD)
+        self.authors = AuthorFactory.create_batch(2)
+        self.categories = CategoryFactory.create_batch(5)
+        self.books = BookFactory.create_batch(5)
+        [UserBookRelationFactory(user=user, book=book) for book in self.books for user in (self.user, self.admin)]
 
     def tearDown(self):
         pass
 
     def test_book_list_load(self):
-        response = client.get(reverse('api:v1:book-list'))
+        response = self.client.get(reverse('api:v1:book-list'))
         self.assertEqual(response.status_code, status.HTTP_200_OK, "Book list failed to load")
 
     def test_book_detail_load(self):
-        response = client.get(reverse('api:v1:book-detail', args=(1,)))
+        book = random.choice(self.books)
+        response = self.client.get(reverse('api:v1:book-detail', args=(book.id,)))
         self.assertEqual(response.status_code, status.HTTP_200_OK, "Book detail failed to load")
 
     def test_book_create_user_unauthorized(self):
-        response = client.post(reverse('api:v1:book-list'), {
-            'title': 'Book4',
-            'title_original': "Book4",
-            'author': 1,
-            'categories': (1, 2)
-        })
+        new_book = BookFactory.build()
+        response = self.client.post(reverse('api:v1:book-list'), BookSerializer(new_book).data)
         self.assertEqual(
             response.status_code, status.HTTP_403_FORBIDDEN,
             "Attempting to create a book as unauthorized user should return 403 Forbidden"
         )
 
     def test_book_create_user_regular(self):
-        client.login(username='user1', password='123123qwe')
-        response = client.post(reverse('api:v1:book-list'), {
-            'title': 'Book4',
-            'title_original': "Book4",
-            'author': 1,
-            'categories': (1, 2)
-        })
+        self.client.force_authenticate(user=self.user)
+        new_book = BookFactory.build()
+        response = self.client.post(reverse('api:v1:book-list'), BookSerializer(new_book).data)
         self.assertEqual(
             response.status_code, status.HTTP_403_FORBIDDEN,
             "Attempting to create a book as regular user should return 403 Forbidden"
         )
 
     def test_book_create_user_admin(self):
-        client.login(username='admin', password='123123qwe')
-        response = client.post(reverse('api:v1:book-list'), {
-            'title': 'Book4',
-            'title_original': "Book4",
-            'author': 1,
-            'categories': (1, 2)
-        })
+        self.client.force_authenticate(user=self.admin)
+        new_book = BookFactory.build()
+        response = self.client.post(reverse('api:v1:book-list'), BookSerializer(new_book).data)
         self.assertEqual(
             response.status_code, status.HTTP_201_CREATED,
             "Attempting to create a book as admin should return 201 Created"
         )
         new_id = response.json()['id']
-        response = client.get(reverse('api:v1:book-detail', args=(new_id,)))
-        self.assertEqual(response.status_code, status.HTTP_200_OK, "Failed to access newly created book")
+        expected_data = BookSerializer(Book.objects.get(id=new_id)).data
+        expected_data['id'] = None
+        self.assertEqual(
+            BookSerializer(new_book).data, expected_data,
+            "Data mismatch in newly created book"
+        )
 
     def test_book_delete_user_unauthorized(self):
-        response = client.delete(reverse('api:v1:book-detail', args=(1,)))
+        book = random.choice(self.books)
+        response = self.client.delete(reverse('api:v1:book-detail', args=(book.id,)))
         self.assertEqual(
             response.status_code, status.HTTP_403_FORBIDDEN,
             "Attempting to delete a book as unauthorized user should return 403 Forbidden"
         )
 
     def test_book_delete_user_regular(self):
-        client.login(username='user1', password='123123qwe')
-        response = client.delete(reverse('api:v1:book-detail', args=(1,)))
+        self.client.force_authenticate(self.user)
+        book = random.choice(self.books)
+        response = self.client.delete(reverse('api:v1:book-detail', args=(book.id,)))
         self.assertEqual(
             response.status_code, status.HTTP_403_FORBIDDEN,
             "Attempting to delete a book as regular user should return 403 Forbidden"
         )
 
     def test_book_delete_user_admin(self):
-        client.login(username='admin', password='123123qwe')
-        response = client.delete(reverse('api:v1:book-detail', args=(1,)))
+        self.client.force_authenticate(self.admin)
+        book = random.choice(self.books)
+        response = self.client.delete(reverse('api:v1:book-detail', args=(book.id,)))
         self.assertEqual(
             response.status_code, status.HTTP_204_NO_CONTENT,
             "Attempting to delete a book as admin should return 204 No Content"
         )
-        response = client.get(reverse('api:v1:book-detail', args=(1,)))
-        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND, "Failed to properly delete a book")
+        deleted_book = Book.objects.filter(id=book.id).first()
+        self.assertIsNone(deleted_book, "Failed to properly delete a book")
 
     def test_book_update_user_unauthorized(self):
-        response = client.patch(reverse('api:v1:book-detail', args=(1,)), {
+        book = random.choice(self.books)
+        response = self.client.patch(reverse('api:v1:book-detail', args=(book.id,)), {
             'title': 'TitleModified'
         })
         self.assertEqual(
@@ -117,8 +103,9 @@ class BooksEndpointTestCase(APITestCase):
         )
 
     def test_book_update_user_regular(self):
-        client.login(username='user1', password='123123qwe')
-        response = client.patch(reverse('api:v1:book-detail', args=(1,)), {
+        self.client.force_authenticate(self.user)
+        book = random.choice(self.books)
+        response = self.client.patch(reverse('api:v1:book-detail', args=(book.id,)), {
             'title': 'TitleModified'
         })
         self.assertEqual(
@@ -127,13 +114,14 @@ class BooksEndpointTestCase(APITestCase):
         )
 
     def test_book_update_user_admin(self):
-        client.login(username='admin', password='123123qwe')
-        response = client.patch(reverse('api:v1:book-detail', args=(1,)), {
+        self.client.force_authenticate(self.admin)
+        book = random.choice(self.books)
+        response = self.client.patch(reverse('api:v1:book-detail', args=(book.id,)), {
             'title': 'TitleModified'
         })
         self.assertEqual(
-            response.status_code, status.HTTP_204_NO_CONTENT,
-            "Attempting to update a book as admin should return 204 No Content"
+            response.status_code, status.HTTP_200_OK,
+            "Attempting to update a book as admin should return 200 OK"
         )
-        response = client.get(reverse('api:v1:book-detail', args=(1,)))
-        self.assertEqual(response.json()['title'], 'TitleModified', "Failed to properly modify resource")
+        updated_book = Book.objects.get(id=book.id)
+        self.assertEqual(updated_book.title, 'TitleModified', "Failed to properly modify resource")
